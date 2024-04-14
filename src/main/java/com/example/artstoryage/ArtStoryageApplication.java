a5e6c67f6da006efc936370ca6092c0cc923ad55
SIWON990327 <pos06058@naver.com>
2024-04-14 11:36:38 +0900

    :tada: 프로젝트 생성

---
+++ src/main/java/com/example/artstoryage/ArtStoryageApplication.java
@@ -0,0 +1,13 @@
+package com.example.artstoryage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtStoryageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArtStoryageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/artstoryage/ArtStoryageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.artstoryage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtStoryageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
