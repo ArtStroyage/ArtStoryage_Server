47c160b965ba9dc48fd8887859fd153ea959a54d
김시원 / Kim Siwon <114036799+SIWON990327@users.noreply.github.com>
2024-04-14 14:46:37 +0900

    :sparkles:[Feat] : Swagger 적용 및 CORS 처리 (#6)

---
+++ src/main/java/com/example/artstoryage/config/SwaggerConfig.java
@@ -0,0 +1,41 @@
+package com.example.artstoryage.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityScheme.Type;
+import io.swagger.v3.oas.models.servers.Server;
+
+@Configuration
+public class SwaggerConfig {
+
+  @Bean
+  public OpenAPI artStoryageAPI() {
+    Info info =
+        new Info().title("ArtStoryage API").description("ArtStoryage API 명세").version("0.0.1");
+
+    String jwtSchemeName = "JWT";
+    SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
+
+    Components components =
+        new Components()
+            .addSecuritySchemes(
+                jwtSchemeName,
+                new SecurityScheme()
+                    .name(jwtSchemeName)
+                    .type(Type.HTTP)
+                    .scheme("Bearer")
+                    .bearerFormat("JWT"));
+
+    return new OpenAPI()
+        .addServersItem(new Server().url("/"))
+        .info(info)
+        .addSecurityItem(securityRequirement)
+        .components(components);
+  }
+}
---
+++ src/main/java/com/example/artstoryage/exception/GlobalException.java
@@ -0,0 +1,11 @@
+package com.example.artstoryage.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class GlobalException extends RuntimeException {
+
+  private final GlobalErrorCode errorCode;
+}
