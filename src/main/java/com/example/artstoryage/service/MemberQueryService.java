c9a7c2390b786edebea97f494b9f6e2a29dde19d
김시원 / Kim Siwon <114036799+SIWON990327@users.noreply.github.com>
2024-04-14 20:50:38 +0900

    :sparkles:[Feat]: AuthMember Argument Resolver 구현 (#14)

---
+++ src/main/java/com/example/artstoryage/service/MemberQueryService.java
@@ -0,0 +1,7 @@
+package com.example.artstoryage.service;
+
+import com.example.artstoryage.domain.member.Member;
+
+public interface MemberQueryService {
+  Member findMemberById(Long memberId);
+}
---
+++ src/main/java/com/example/artstoryage/service/impl/MemberQueryServiceImpl.java
@@ -0,0 +1,26 @@
+package com.example.artstoryage.service.impl;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.artstoryage.domain.member.Member;
+import com.example.artstoryage.exception.GlobalErrorCode;
+import com.example.artstoryage.exception.custom.MemberException;
+import com.example.artstoryage.repository.MemberRepository;
+import com.example.artstoryage.service.MemberQueryService;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class MemberQueryServiceImpl implements MemberQueryService {
+  private final MemberRepository memberRepository;
+
+  @Override
+  public Member findMemberById(Long memberId) {
+    return memberRepository
+        .findById(memberId)
+        .orElseThrow(() -> new MemberException(GlobalErrorCode.MEMBER_NOT_FOUND));
+  }
+}
